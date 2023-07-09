221e07152597d511380706493c4ed443f3af3463
Daniel Stagg <50230774+T0MIS0N@users.noreply.github.com>
2023-07-08 19:36:03 -0600

    Fix Target Set Bug
    Fix bug where target isn't set to a newly selected target for the spaceship flee mechanic.

--- Level/test_scene.gd
+++ Level/test_scene.gd
@@ -3,11 +3,8 @@ extends Node3D
 @onready var top_left_post = $TopLeftPost
 @onready var bottom_right_post = $BottomRightPost
 @onready var spaceship = $Spaceship
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+@onready var camera = $Camera3D
+@onready var obstacle = $Obstacle
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -17,25 +14,19 @@ func _process(delta):
 
 func keep_within_boundaries():
 	if spaceship.position.x < top_left_post.position.x:
-		print("LEFT")
 		spaceship.position.x = bottom_right_post.position.x - 1
 	
 	if spaceship.position.x > bottom_right_post.position.x:
-		print("RIGHT")
 		spaceship.position.x = top_left_post.position.x + 1
 	
 	if spaceship.position.z < top_left_post.position.z:
-		print("TOP")
 		spaceship.position.z = bottom_right_post.position.z - 1
 		
 	if spaceship.position.z > bottom_right_post.position.z:
-		print("BOTTOM")
 		spaceship.position.z = top_left_post.position.z + 1
 
 
 func _input(event):
 	if event is InputEventMouse:
-		var pos3d = $Camera3D.project_position(event.position,$Camera3D.position.y)
-		$Obstacle.position = pos3d
-		if $Obstacle.position.y > 0:
-			print($Obstacle.position)
+		var pos3d = camera.project_position(event.position,camera.position.y)
+		obstacle.position = pos3d
